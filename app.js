36317e81180027406c80b27ca40b0b82c8f5628e
Austin <kaceyk0203@gmail.com>
2020-09-17 16:02:08 +0200

    Finish project section and Footer

--- app.js
+++ app.js
@@ -1,8 +1,92 @@
+/*Variables*/
+const header = document.querySelector(".header");
+const sidebar = document.querySelector(".sidebar");
+const front = document.querySelectorAll(".front")
+const back = document.querySelectorAll(".back")
+const farBack = document.querySelectorAll(".farback")
+const mainCharactor = document.querySelector(".maincharactor")
+const handler = document.getElementById("handler");
+const heroLine = document.querySelector(".hero-line")
+const footerLine = document.querySelector(".contact-line")
+const sectionObjectBack = document.querySelectorAll(".objectback")
+const sectionObjectFront = document.querySelectorAll(".objectfront")
+const sectionObjectFarFront = document.querySelectorAll(".objectfarfront")
+const projectSection1 = document.getElementById("projectsection1")
+const projectSection2 = document.getElementById("projectsection2")
+const projectSection3 = document.getElementById("prjoectsection3")
+
+
+
 /*fullpage transition effect*/
 new fullpage("#fullpage", {
-  autoScrolling: true,
-  dragAndMove: true,
+  scrollingSpeed: 1600,  
+
+  onLeave: function(origin, destination, direction){
+    var loadedSection = this;
+    /*Intro section*/
+    handler.style.marginTop = (-10 +(destination.index * 5) )+ "rem";
+    front.forEach(x => {
+      x.style.bottom = (0 - (destination.index * 1250)) + "px";
+    });
+    back.forEach(x => {
+      x.style.bottom = (0 - (destination.index * 1050)) + "px";
+    });
+    farBack.forEach(x => {
+      x.style.bottom = (0 - (destination.index * 850)) + "px";
+    });
+    mainCharactor.style.bottom = 0 - destination.index * 900 + "px";
+    heroLine.style.top = (192 + (destination.index * 600)) + "px";
+
+    /*Footer*/
+    if(origin.index===3){
+      footerLine.style.top = 15 + "rem";
+    }else{
+      footerLine.style.top = 100 + "rem";
+    };
+
+    /*Project section*/
+    if(destination.index >=1 && destination.index <=3){
+      sectionObjectBack.forEach(x=>{
+        x.style.marginTop = 0;
+      });
+      sectionObjectFront.forEach(x=>{
+        x.style.marginTop = 0;
+      });
+      sectionObjectFarFront.forEach(x=>{
+        x.style.marginTop = 0;
+      })
+    }else{
+      sectionObjectBack.forEach(x=>{
+        x.style.marginTop = 100 + "rem";
+      });
+      sectionObjectFront.forEach(x=>{
+        x.style.marginTop = 150 + "rem";
+      });
+      sectionObjectFarFront.forEach(x=>{
+        x.style.marginTop = 200 + "rem";
+      })
+    };
+
+    if(destination.index === 1){
+      projectSection1.style.top = 0;
+    }else if(destination.index===2){
+      projectSection1.style.top = -80 +"rem";
+      projectSection2.style.top = 0 + "rem";
+    }else if(destination.index===3){
+      projectSection2.style.top = -80 + "rem";
+      projectSection3.style.top = 0 + "rem";
+    }else if(destination.index===4){
+      projectSection3.style.top = -80 + "rem";
+    }else{
+      projectSection1.style.top = 100 + "rem";
+      projectSection2.style.top = 100 + "rem";
+      projectSection3.style.top = 100 + "rem";
+    }
+
+  }
 });
+
+
 
 /*parallax effect*/
 document.addEventListener("mousemove", parallax);
@@ -15,19 +99,15 @@ function parallax(e) {
 
     layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
   });
+  this.querySelectorAll(".background-object").forEach((backgroundObject) => {
+    const speed = backgroundObject.getAttribute("data-speed");
+
+    const x = (window.innerWidth - e.pageX * speed) / 70;
+    const y = (window.innerHeight - e.pageY * speed) / 70;
+
+    backgroundObject.style.transform = `translateX(${x}px) translateY(${y}px)`;
+  });
 }
 
-/*header transition*/
 
-const header = document.querySelector('.header');
-const sectionOne = document.querySelector('.s1');
 
-const sectionOneOptions = {};
-
-const sectionOneObserver = new IntersectionObserver(function(entries,sectionOneObserver){
-    entries.forEach(entry=>{
-       console.log(entry.target)
-    })
-},sectionOneOptions);
-
-sectionOneObserver.observe(sectionOne);

4a7711ced339ab573ec60429c40489238439b5b1
Austin <kaceyk0203@gmail.com>
2020-10-14 17:10:38 +0200

    Modify about page, index page

--- JS/about.js
+++ JS/about.js
@@ -29,5 +29,7 @@ t1.from(rule, {cssRule:{scale:0, transformOrigin:"center center"}})
 
 if(windowMedia.matches){
     let t2 = gsap.timeline({defaults:{duration:.8}})
-    t2.from(".main-img",{y:100, opacity:0.5, delay:1.2})
+    t2.from(".main-img",{y:100, opacity:0.5, delay:1.3})
+    .from('.intro-text', {x:-50, opacity:0}, "-=0.8")
+    .from('.intro-list', {x:50, opacity:0, stagger:0.3}, "-=0.6")
 }
